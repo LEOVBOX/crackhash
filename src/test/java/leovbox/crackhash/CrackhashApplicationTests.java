de3f1fb83004fa8e342462671bbbbe8bf3c327d9
Шайхутдинов Леонид <leovbox@mail.ru>
2025-02-21 15:15:38 +0700

    Initial commit

---
+++ src/test/java/leovbox/crackhash/CrackhashApplicationTests.java
@@ -0,0 +1,13 @@
+package leovbox.crackhash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrackhashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
